e7c0637ad0017d0f6923daedb1560e4211f58995
CungToan <23hiddengem@gmail.com>
2022-05-21 23:44:26 +0700

    update

--- bootstrap-js/script.js
+++ bootstrap-js/script.js
@@ -3,13 +3,22 @@ const modalBox =document.getElementById('modal-box');
 const closeBtn = document.querySelector('.close');
 const escBtn = document.querySelector('.escape-btn')
 
+
+function backgroundDefault(){
+    document.body.style.backgroundColor = '#fff';
+}
+function overlayBg(){
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+}
 function activeBox(){
     modalBox.classList.add('activeModalBox');
+    overlayBg();
 }
 
 function removeBox(){
     modalBox.classList.remove('activeModalBox');
     modalBox.classList.add('removeModalBox');
+    backgroundDefault()
 
 }
 
